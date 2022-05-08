47d0f61ef3827a0a25d7715f8758cd4185491cf1
HubertFijolek1 <91610603+HubertFijolek1@users.noreply.github.com>
2022-05-08 11:17:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+'''welcome'''
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    '''welcome repeat'''
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
